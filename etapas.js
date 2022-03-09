4f8e983c060d68009d3395411d1382a643d4137d
Magalhaes <marmagalhaes@deloitte.com>
2022-03-09 12:24:53 -0300

    fake api

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '1.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 72245,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '2.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: '3.jpg', legenda: 'Prefeito'},
+                    {url: '4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Beltrano',
+                partido: 'DEFG',
+                vice: 'Bel',
+                fotos:[
+                    {url: '5.jpg', legenda: 'Prefeito'},
+                    {url: '6.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+
+]
